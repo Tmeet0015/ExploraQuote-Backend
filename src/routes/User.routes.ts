2489cb06ae3fddcce021908513ed7acea954d30e
Juzer Hajoori <jhajoorigmail.com>
2024-12-28 01:03:14 +0530

    user route

--- src/routes/User.routes.ts
+++ src/routes/User.routes.ts
@@ -1,13 +1,33 @@
 import express from 'express';
 import { authMiddleware } from '../middleware/jwt';
-import { login, getUserProfileById } from '../controllers/user.controller';
-
+import {
+    login,
+    changePassword,
+    getUserProfileData,
+    editUserProfile,
+    getUserProfileById,
+    deleteUser,
+  } from "../controllers/user.controller";
 
 const UserRouter = express.Router();
 
-UserRouter.post('/login', login);
-UserRouter.get('/get-user-profile-by-Id/:Id', authMiddleware, getUserProfileById);
 
+// User & Admin Login
+UserRouter.post("/login", login);
 
+// Change Password
+UserRouter.post("/change-password",authMiddleware, changePassword);
+
+// Get Logged-in User Profile
+UserRouter.get("/profile",authMiddleware, getUserProfileData);
+
+// Edit Logged-in User Profile
+UserRouter.put("/profile",authMiddleware, editUserProfile);
+
+// Get a User Profile by ID
+UserRouter.get("/:Id",authMiddleware, getUserProfileById);
+
+// Delete a User
+UserRouter.delete("/:Id",authMiddleware, deleteUser);
 
 export default UserRouter;
